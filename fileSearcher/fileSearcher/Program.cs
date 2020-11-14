877621072434b5c05b65c7d0a79998634d5ed1b1
spoonLead <xuxlaevoleg@yandex.ru>
2020-11-14 19:20:30 +0300

    Added new project VS files

---
+++ fileSearcher/fileSearcher/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace fileSearcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
