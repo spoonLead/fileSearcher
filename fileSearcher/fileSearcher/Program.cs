5293cb3a2f2afaded304e10c768fb0be4561cc9c
spoonLead <xuxlaevoleg@yandex.ru>
2020-11-16 13:50:11 +0300

    Feat: Added method for printing text with text parameters

--- fileSearcher/fileSearcher/Program.cs
+++ fileSearcher/fileSearcher/Program.cs
@@ -6,7 +6,69 @@ namespace fileSearcher
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            Console.ReadKey();
+
+        }
+
+
+        static void printText(string str, string side = "left")
+        {
+            int ost = Console.BufferWidth - str.Length;  //остаток символов строки с сообщением
+            if (side == "center")
+            {
+                //задание пробелов перед сообщением, для его отображения по центру
+                Console.Write(" ");
+                for (int i = 0; i < ost / 2 - 1; i++)   // -1 для звёздочки в начале
+                {
+                    Console.Write(" ");
+                }
+
+
+                Console.Write($"{str}");
+
+
+                //задание пробелов после сообщениея, для его отображения по центру
+
+                if (ost % 2 == 0)
+                {
+                    for (int i = 0; i < ost / 2 - 1; i++)
+                    {
+                        Console.Write(" ");
+                    }
+                    Console.Write(" ");
+                }
+                else
+                {
+                    for (int i = 0; i < ost / 2; i++)
+                    {
+                        Console.Write(" ");
+                    }
+                    Console.Write(" ");
+                }
+            }
+            else if (side == "left")
+            {
+                Console.Write(" ");
+                Console.Write($"{str}");
+
+
+                //задание пробелов после сообщениея, для отображения правой границы
+
+                for (int i = 0; i < ost - 2; i++)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write(" ");
+
+            }
+            else if (side == "right")
+            {
+                //задание пробелов перед сообщением, для его отображения по центру
+                Console.Write(" ");
+                Console.SetCursorPosition(Console.BufferWidth - 1 - str.Length, Console.CursorTop);
+                Console.Write($"{str} ");
+            }
         }
     }
 }
