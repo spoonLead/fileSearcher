82ac9a62930bbe445787083d0c1bb524cdfb28a3
spoonLead <xuxlaevoleg@yandex.ru>
2020-11-19 17:31:30 +0300

    Chore

--- fileSearcher/ThreadingText/Program.cs
+++ fileSearcher/ThreadingText/Program.cs
@@ -8,30 +8,30 @@ namespace ThreadingText
     class Program
     {
         public static int enter = 0;
+        static AutoResetEvent are = new AutoResetEvent(false);
 
         static void Main(string[] args)
         {
-            //Thread printingNums = new Thread(printNums);
-            // ManualResetEvent me = new ManualResetEvent(true);
-            //printingNums.Start(me);
+            Thread printingNums = new Thread(printNums);
+            printingNums.Start();
 
-            printNums();
 
             while (true)
             {
                 enter = Convert.ToInt32(Console.ReadLine());
-                
+                if (enter == 2)
+                    are.Set();
             }
         }
 
-        async static void printNums()
+        static void printNums()
         {
             int i = 0;
             while (i <= 100)
             {
                 if (enter == 1)
-                    await Task.Delay(1);
-                if(enter != 1)
+                    are.WaitOne();
+                if (enter != 1)
                 {
                     int cl = Console.CursorLeft;
                     int ct = Console.CursorTop;
@@ -40,11 +40,29 @@ namespace ThreadingText
                     Console.WriteLine(i);
                     Console.SetCursorPosition(cl, ct);
 
-                    await Task.Delay(1000);
+                    System.Threading.Thread.Sleep(1000);
                     i++;
                 }
             }
-            
+
+            /*for(int i = 0; i < 100; i++)
+            {
+                if (enter == 2)
+                    are.WaitOne();
+                if (enter == 1)
+                {
+                    int cl = Console.CursorLeft;
+                    int ct = Console.CursorTop;
+
+                    Console.SetCursorPosition(0, 10);
+                    Console.WriteLine(i);
+                    Console.SetCursorPosition(cl, ct);
+
+                    System.Threading.Thread.Sleep(1000);
+                    Console.WriteLine(i);
+                }
+            }*/
+
         }
     }
 }
